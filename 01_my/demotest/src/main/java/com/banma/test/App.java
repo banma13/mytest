63f2ac4d5e51afe2cc9ecdd6741a2e25745abee5
banma <912794823@qq.com>
2018-11-30 16:12:41 +0800

    第一个项目

---
+++ 01_my/demotest/src/main/java/com/banma/test/App.java
@@ -0,0 +1,11 @@
+package com.banma.test;
+
+/**
+ * User: BanMa
+ * DATE: 2018/11/30
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("------111------");
+    }
+}
